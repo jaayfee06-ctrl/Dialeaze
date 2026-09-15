6ca064b83845fcc4c6b6df0b6aa883fb3ba54b35
Junaid Sabir <jaayfee06@gmail.com>
2026-09-15 12:23:38 -0700

    Revert "Use assigned Dialeaze number for all customers"
    This reverts commit c49272e447b8e62a28c5aaeddb57452e8b2435b9.

--- public/app.js
+++ public/app.js
@@ -1275,8 +1275,8 @@ async function loadCustomerAccount() {
         if (businessNumber) {
 
             const displayNumber =
-    customerAccount.assignedPhoneNumber ||
-"";
+    customerAccount.phoneNumber ||
+    "";
 
             businessNumber.textContent =
                 displayNumber
@@ -1297,8 +1297,8 @@ async function loadCustomerAccount() {
         ) {
 
             const displayNumber =
-    customerAccount.assignedPhoneNumber ||
-"";
+    customerAccount.phoneNumber ||
+    "";
 
 
             messagesBusinessNumber.textContent =
@@ -2843,8 +2843,8 @@ if (digits.length === 10) {
     destinationNumber:
         number,
     callerNumber:
-    customerAccount.assignedPhoneNumber ||
-    ""
+        customerAccount.phoneNumber ||
+        ""
 })
                         }
                     );
@@ -2901,7 +2901,7 @@ if (digits.length === 10) {
             userVariables: {
                 destination: number,
                 callerNumber:
-    customerAccount.assignedPhoneNumber || "",
+                    customerAccount.phoneNumber || "",
                 usageId:
                     currentOutboundUsageId || null
             }
@@ -5248,7 +5248,7 @@ async function sendMessage() {
                 "outbound",
 
             from:
-    customerAccount.assignedPhoneNumber,
+                customerAccount.phoneNumber,
 
             to:
                 recipient,
