51e9c874622b7677711d5b6e1edd2d61c0042476
Junaid Sabir <jaayfee06@gmail.com>
2026-09-17 19:47:06 -0700

    Fix recording destination number lookup

--- server.js
+++ server.js
@@ -8807,7 +8807,7 @@ app.get("/api/call-recordings", async (req, res) => {
                     `&user_id=eq.${encodeURIComponent(
                         userId
                     )}` +
-                   `&select=id,caller_number,dialed_number`,
+                   `&select=id,caller_number,destination_number`,
                     {
                         method: "GET",
                         headers: {
@@ -8875,7 +8875,7 @@ app.get("/api/call-recordings", async (req, res) => {
     null,
 
 dialed_number:
-    usage?.dialed_number ||
+    usage?.destination_number ||
     recording.dialed_number ||
     null
                 };
